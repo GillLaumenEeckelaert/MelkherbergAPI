47d0d165f8a09d8a78356b60d4f000d9c016218a
Gill Eeckelaert <gill.eeckelaert@telenet.be>
2026-02-07 13:58:34 +0100

    Add files

---
+++ API/Program.cs
@@ -0,0 +1,24 @@
+﻿using Database;
+using Framework.Build;
+using Handlers.Shows;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddMvc()
+    .AddApplicationPart(typeof(GetEventsHandler).Assembly);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        x => x.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+});
+
+builder.Services.AddAutoMapper(cfg => { }, typeof(GetEventsHandler));
+
+Builder.FullBuild(builder);
